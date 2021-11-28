ea024e63e33b356ad8dca33657e761a59872a9a5
shinshin86 <beagles1986@gmail.com>
2021-11-28 13:15:20 +0900

    Add constants.ts

---
+++ constants.ts
@@ -0,0 +1,25 @@
+const RESET_COLOR = "\x1b[0m";
+const FONT_COLOR_RED = "\x1b[31m";
+const FONT_COLOR_GREEN = "\x1b[32m";
+const FONT_COLOR_CYAN = "\x1b[36m";
+const TABLE_CHARS = {
+  topMid: "┬",
+  topLeft: "┌",
+  topRight: "┐",
+  bottomMid: "┴",
+  bottomLeft: "└",
+  bottomRight: "┘",
+  midMid: "┼",
+  leftMid: "├",
+  rightMid: "┤",
+  horizontalLine: "─",
+  verticalLine: "│",
+};
+
+export {
+  FONT_COLOR_CYAN,
+  FONT_COLOR_GREEN,
+  FONT_COLOR_RED,
+  RESET_COLOR,
+  TABLE_CHARS,
+};
--- display.ts
+++ display.ts
@@ -1,22 +1,5 @@
 import { Task } from "./interface.ts";
-
-const RESET_COLOR = "\x1b[0m";
-const FONT_COLOR_RED = "\x1b[31m";
-const FONT_COLOR_GREEN = "\x1b[32m";
-const FONT_COLOR_CYAN = "\x1b[36m";
-const TABLE_CHARS = {
-  topMid: "┬",
-  topLeft: "┌",
-  topRight: "┐",
-  bottomMid: "┴",
-  bottomLeft: "└",
-  bottomRight: "┘",
-  midMid: "┼",
-  leftMid: "├",
-  rightMid: "┤",
-  horizontalLine: "─",
-  verticalLine: "│",
-};
+import { TABLE_CHARS } from "./constants.ts";
 
 function getChars(width: number, str: string): string {
   return Array.from(Array(width), () => str).join("");
